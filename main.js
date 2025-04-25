cb338ff3ab30e84427596624ae0c3f19321428b0
giovana <135229253+SandraeGiovana@users.noreply.github.com>
2025-04-25 09:36:50 -0300

    25

--- main.js
+++ main.js
@@ -9,16 +9,16 @@ for (let i = 0; i < botoes.length; i++) {
             textos[j].classList.remove("ativo");
         }
 
-        botoes[i].classList.add("ativo");
+        botoes[i].classList.add("ativo");343458
         textos[i].classList.add("ativo");
     }
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
+const tempoObjetivo1 = new Date("2025-04-30T00:00:00");
+const tempoObjetivo2 = new Date("2025-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2026-01-30T00:00:00");
+const tempoObjetivo4 = new Date("2025-07-15T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
